0a7d07e61971530197490feeea13a6fedf50836a
rgrp <rufus.pollock@okfn.org>
2008-11-01 22:21:36 +0000

    [all][m]: more refactoring for genericism.
      * lib/base.py, templates: Create site_title config parameter and use it in templates (so can have Open Milton instead of Open Shakespeare)
      * templates: Remove references to Shakespeare where possible in templates (will need to some more elaborate stuff for index and guide)
      * Refactor load_texts stuff to be more generic (remove from cli.py and put in shksprdata using paster command reffed from setup.py)
      * miltondata/cli.py: LoadTexts command.

--- shksprdata/load.py
+++ shksprdata/load.py
@@ -2,21 +2,37 @@ from ConfigParser import SafeConfigParser
 
 import pkg_resources
 
-import shakespeare.model as model
+import shakespeare.cli
 
-def load_texts():
-    pkg = 'shksprdata'
-    fileobj = pkg_resources.resource_stream(pkg, '/texts/metadata.txt')
-    cfgp = SafeConfigParser()
-    cfgp.readfp(fileobj)
-    for section in cfgp.sections():
-        item = model.Material.byName(section)
-        if item is None:
-            item = model.Material(name=section)
-        assert item is not None
-        for key, val in cfgp.items(section):
-            setattr(item, key, val)
-        item.src_pkg = pkg
-        item.src_locator = '/texts/%s.txt' % section
-        model.Session.flush()
+class LoadTexts(shakespeare.cli.BaseCommand):
+    '''Load shakespeare texts.
+    '''
+    summary = __doc__.split('\n')[0]
+    usage = __doc__
+    max_args = None
+    min_args = 0
+
+    def command(self):
+        self._load_config()
+        self.load_texts()
+        print 'Loaded successfully'
+
+    @classmethod
+    def load_texts(self):
+        import shakespeare.model as model
+        pkg = 'shksprdata'
+        fileobj = pkg_resources.resource_stream(pkg, '/texts/metadata.txt')
+        cfgp = SafeConfigParser()
+        cfgp.readfp(fileobj)
+        for section in cfgp.sections():
+            item = model.Material.byName(section)
+            if item is None:
+                item = model.Material(name=section)
+            assert item is not None
+            for key, val in cfgp.items(section):
+                setattr(item, key, val)
+            item.src_pkg = pkg
+            item.src_locator = '/texts/%s.txt' % section
+            model.Session.flush()
+
 
